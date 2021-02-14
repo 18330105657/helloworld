05c5ae8245f8ab98446e7e502ae69abcaf0d59ea
tianxj <2824152664@qq.com>
2021-02-14 15:12:06 +0800

    pom、yml文件配置

--- childdemo/src/main/java/com/example/childdemo/web/helloworld.java
+++ childdemo/src/main/java/com/example/childdemo/web/helloworld.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/parent")
+@RequestMapping("/child")
 public class helloworld {
 
     @RequestMapping("helloworld")
--- src/main/java/com/example/helloworld/web/helloworld.java
+++ src/main/java/com/example/helloworld/web/helloworld.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/helloworld")
+@RequestMapping("/parent")
 public class helloworld {
 
     @RequestMapping("helloworld")
