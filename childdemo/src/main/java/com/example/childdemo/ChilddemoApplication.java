9a054faf001be106f9556b2d324ea3d094f6b771
tianxj <2824152664@qq.com>
2021-02-14 13:49:47 +0800

    md文件的使用

---
+++ childdemo/src/main/java/com/example/childdemo/ChilddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.childdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChilddemoApplication.class, args);
+	}
+
+}
---
+++ childdemo/src/test/java/com/example/childdemo/ChilddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
