34a8c428a58ff6bdb3aa9e98c2787cba8bfc63c1
tianxj <2824152664@qq.com>
2021-02-14 13:02:00 +0800

    子工程

---
+++ childdemo/src/main/java/com/example/childdemo/web/helloworld.java
@@ -0,0 +1,15 @@
+package com.example.childdemo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parent")
+public class helloworld {
+
+    @RequestMapping("helloworld")
+    public String helloworld()
+    {
+        return "helloworld";
+    }
+}
